c0f7b7c83b2d6c3a3d4f4d0ee0b69676a17a6849
Nico Park <nicopark@loemhouse.com>
2026-07-26 22:41:00 +0800

    fix product type mapping

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -4,65 +4,114 @@ import { getProducts } from "@/lib/productStore";
 
 type Props = {
   params: Promise<{
-    id:string;
+    id: string;
   }>;
 };
 
 
 export default async function ProductDetailPage({
   params,
-}:Props){
+}: Props) {
 
 
-const {id}=await params;
+  const { id } = await params;
 
 
-// 从数据库读取
-const products = await getProducts();
+  // 从数据库读取
+  const products = await getProducts();
 
 
-const product = products.find(
-  p=>p.id===id
-);
-
-
-
-if(!product){
-
-return(
-<main className="
-min-h-screen
-bg-black
-p-10
-text-white
-">
-Product Not Found
-</main>
-)
-
-}
+  const rawProduct = products.find(
+    p => p.id === id
+  );
 
 
 
-return (
+  if (!rawProduct) {
 
-<main
-className="
-min-h-screen
-bg-black
-px-6
-py-10
-text-white
-md:px-10
-"
->
+    return (
+      <main
+        className="
+        min-h-screen
+        bg-black
+        p-10
+        text-white
+        "
+      >
+        Product Not Found
+      </main>
+    );
 
-<ProductClient
-product={product}
-/>
+  }
 
-</main>
 
-)
+
+  // 数据库字段 -> 前端 Product 类型转换
+  const product = {
+
+    ...rawProduct,
+
+
+    // snake_case 转 camelCase
+    productCode:
+      rawProduct.product_code,
+
+
+    launchSeason:
+      rawProduct.launch_season,
+
+
+    retailPrice:
+      rawProduct.retail_price,
+
+
+    targetCost:
+      rawProduct.target_cost,
+
+
+    currentCost:
+      rawProduct.current_cost,
+
+
+    coverImage:
+      rawProduct.cover_image,
+
+
+    aiInsights:
+      rawProduct.ai_insights,
+
+
+    // 如果数据库没有，补默认值
+    designer:
+      rawProduct.designer ?? "",
+
+
+    factory:
+      rawProduct.factory ?? "",
+
+  };
+
+
+
+  return (
+
+    <main
+      className="
+      min-h-screen
+      bg-black
+      px-6
+      py-10
+      text-white
+      md:px-10
+      "
+    >
+
+      <ProductClient
+        product={product}
+      />
+
+    </main>
+
+  );
 
 }
